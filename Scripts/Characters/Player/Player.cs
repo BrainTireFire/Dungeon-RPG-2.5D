089c2084fe3fa4f09989c0f30e59eb6ca5a3e999
Maciej Kawęcki <skimiskim12@gmail.com>
2024-05-18 18:45:11 +0200

    Player movement and animation add

---
+++ Scripts/Characters/Player/Player.cs
@@ -0,0 +1,65 @@
+using Godot;
+using System;
+using DungeonRPG2.D.Scripts.General;
+
+public partial class Player : CharacterBody3D
+{
+    [ExportGroup("Required Nodes")]
+    [Export] private AnimationPlayer _animationPlayerNode;
+    [Export] private Sprite3D _spriteNode;
+    
+    private Vector2 _direction = new();
+
+    public override void _Ready()
+    {
+        _animationPlayerNode.Play(GameConstants.ANIM_IDLE);
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Velocity = new (_direction.X, 0, _direction.Y);
+        Velocity *= 5;
+
+        MoveAndSlide();
+        
+        Flip();
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        _direction = Input.GetVector(
+            GameConstants.INPUT_MOVE_LEFT, 
+            GameConstants.INPUT_MOVE_RIGHT, 
+            GameConstants.INPUT_MOVE_FORWARD, 
+            GameConstants.INPUT_MOVE_BACKWARD
+        );
+        
+        if (_direction == Vector2.Zero)
+        {
+            _animationPlayerNode.Play(GameConstants.ANIM_IDLE);
+        }
+        else
+        {
+            _animationPlayerNode.Play(GameConstants.ANIM_MOVE);
+        }
+        
+        
+    }
+
+    private void Flip()
+    {
+        bool isNotMovingHorizontally = Velocity.X == 0;
+
+        if (isNotMovingHorizontally)
+        {
+            return;
+        }
+            
+        bool isMovingLeft = Velocity.X < 0;
+        _spriteNode.FlipH = isMovingLeft;
+    }
+    
+    
+    
+    
+}
---
+++ Scripts/General/GameConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace DungeonRPG2.D.Scripts.General;
+
+public partial class GameConstants
+{
+    // Animations
+    public const string ANIM_IDLE = "Idle";
+    public const string ANIM_MOVE = "Move";
+    
+    // Inputs
+    public const string INPUT_MOVE_LEFT = "MoveLeft";
+    public const string INPUT_MOVE_RIGHT = "MoveRight";
+    public const string INPUT_MOVE_FORWARD = "MoveForward";
+    public const string INPUT_MOVE_BACKWARD = "MoveBackward";
+}
