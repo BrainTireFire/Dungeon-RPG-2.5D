588cf39825c67d9cf9d02e4383e3de62e961e760
Maciej Kawęcki <skimiskim12@gmail.com>
2024-05-28 20:36:31 +0200

    UI, start game menu

---
+++ Scripts/UI/ContainerType.cs
@@ -0,0 +1,11 @@
+﻿namespace DungeonRPG2.D.Scripts.UI;
+
+public enum ContainerType
+{
+    Start,
+    Pause,
+    Victory,
+    Defeat,
+    Stats,
+    Reward,
+}
---
+++ Scripts/UI/UIController.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using DungeonRPG2.D.Scripts.UI;
+
+
+public partial class UIController : Control
+{
+    private Dictionary<ContainerType, UIContainer> _containers;
+
+    public override void _Ready()
+    {
+        _containers = GetChildren()
+            .Where((element) => element is UIContainer )
+            .Cast<UIContainer>()
+            .ToDictionary((element) => element.Container);
+
+        _containers[ContainerType.Start].Visible = true;
+
+        _containers[ContainerType.Start].ButtonNode.Pressed += HandleStartPressed;
+    }
+
+    private void HandleStartPressed()
+    {
+        GetTree().Paused = false;
+        
+        _containers[ContainerType.Start].Visible = false;
+    }
+}
